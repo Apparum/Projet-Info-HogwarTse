5cd21abbe12d3182a2203bb5d547966a798a1c31
PEREZ Hanaé <hanae.perez@univ-st-etienne.fr>
2017-10-17 10:46:44 +0200

    Init repo

---
+++ src/main/java/Helloworld.java
@@ -0,0 +1,11 @@
+import org.joda.time.LocalTime;
+
+public class Helloworld {
+	
+	public static void main(String[] args) {
+		
+		LocalTime currentTime = new LocalTime();
+		System.out.println("Time = " + currentTime);
+	}
+
+}
